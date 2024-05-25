1da0eac90010d7e435375a4c1ebda94d1b981e02
Valery-S <valerij-01@mail.ru>
2024-05-25 13:11:59 +0300

    Add project files.

---
+++ MarketParser/Program.cs
@@ -0,0 +1,13 @@
+﻿using PuppeteerSharp;
+
+namespace MarketParser
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            
+        }
+    }
+}
