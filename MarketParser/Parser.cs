88e133329ccedc6ff27bf6eedffd30bfc82a9345
Valery-S <valerij-01@mail.ru>
2024-05-25 14:11:29 +0300

    Парсер выделен в класс

---
+++ MarketParser/Parser.cs
@@ -0,0 +1,75 @@
+﻿using PuppeteerSharp;
+
+namespace MarketParser
+{
+    internal class Parser
+    {
+        private static BrowserFetcher? browserFetcher = null;
+        private static IBrowser? browser = null;
+
+        public Parser() {}
+
+        //Открытие браузера
+        public async Task StartBrowser() 
+        {
+            browserFetcher ??= new BrowserFetcher();
+            await browserFetcher.DownloadAsync();
+            browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = true
+            });
+        }
+
+        //Закрытие браузера
+        public async Task CloseBrowser()
+        {
+            await browser?.CloseAsync();
+        }
+
+
+        //Получение информации о товаре
+        public async Task GetContent()
+        {
+            // Загрузка страницы 
+            var page = await browser?.NewPageAsync();
+            await page.GoToAsync("https://www.citilink.ru/search/?text=смартфон&sorting=price_asc&p=3");
+
+            // Получение содержимого страницы
+            var content = await page.GetContentAsync();
+
+            // Запрос на нахождение цен товаров
+            var jsSelectAllPrices = "Array.from(document.querySelectorAll(\".e1j9birj0\")).map(a => a.innerText);";
+
+            // Поиск содержимого по запросу
+            var prices = await page.EvaluateExpressionAsync<string[]>(jsSelectAllPrices);
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                Console.WriteLine("{0}", prices[i]);
+            }
+
+            SaveHtmlToFile(content);
+        }
+
+
+        // Сохранение html кода страницы в файл
+        void SaveHtmlToFile(string? content)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter("D:\\Test5.html");
+                sw.WriteLine(content);
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+
+    }
+}
--- MarketParser/Program.cs
+++ MarketParser/Program.cs
@@ -1,54 +1,15 @@
-﻿using PuppeteerSharp;
-
-namespace MarketParser
+﻿namespace MarketParser
 {
     internal class Program
     {
         static async Task Main(string[] args)
         {
-            var browserFetcher = new BrowserFetcher();
-            await browserFetcher.DownloadAsync();
-            var browser = await Puppeteer.LaunchAsync(new LaunchOptions
-            {
-                Headless = true
-            });
+            Parser parser = new Parser();
 
-            // Загрузка страницы 
-            var page = await browser.NewPageAsync();
-            await page.GoToAsync("https://www.citilink.ru/search/?text=%D1%81%D0%BC%D0%B0%D1%80%D1%82%D1%84%D0%BE%D0%BD");
+            await parser.StartBrowser();
+            await parser.GetContent();
+            await parser.CloseBrowser();
 
-            // Получение содержимого страницы
-            var content = await page.GetContentAsync();
-
-            // Запрос на нахождение цен товаров
-            var jsSelectAllPrices = "Array.from(document.querySelectorAll(\".e1j9birj0\")).map(a => a.innerText);";
-
-            // Поиск содержимого по запросу
-            var prices = await page.EvaluateExpressionAsync<string[]>(jsSelectAllPrices);
-
-            for (int i = 0; i < prices.Length; i++)
-            {
-                Console.WriteLine("{0}", prices[i]);
-            }
-
-            // Сохранение html кода страницы в файл
-            try
-            {
-                StreamWriter sw = new StreamWriter("D:\\Test.html");
-                sw.WriteLine(content);
-                sw.Close();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception: " + e.Message);
-            }
-            finally
-            {
-                Console.WriteLine("Executing finally block.");
-            }
-
-            // Закрытие браузера
-            await browser.CloseAsync();
         }
     }
 }
